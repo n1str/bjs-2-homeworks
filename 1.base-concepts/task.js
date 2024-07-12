a593bcdb8c805be699a22433b05eae75602c9045
nex3g <hardminesu@gmail.com>
2024-07-13 00:00:30 +0300

    homework1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    return [];
+  } else if (d === 0) {
+    
+    return [-b / (2 * a)];
+  } else {
+    
+    return [
+      (-b + Math.sqrt(d)) / (2 * a),
+      (-b - Math.sqrt(d)) / (2 * a),
+    ];
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyPercent = (percent / 100) / 12;
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / ((1 + monthlyPercent) ** countMonths - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  return parseFloat(totalAmount.toFixed(2));
 }
+
