bdea5da2e267c78eac3d8c7775ee5785a3be86f4
nex3g <hardminesu@gmail.com>
2024-07-13 01:27:23 +0300

    homework3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = []; 
+  }
 
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  };
   
-}
+  Student.prototype.addMarks = function(...marksToAdd) {
+    if (this.marks) {
+      this.marks.push(...marksToAdd);
+    }
+  };
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+  Student.prototype.getAverage = function() {
+    if (this.marks && this.marks.length > 0) {
+      return this.marks.reduce((sum, mark) => sum + mark, 0) / this.marks.length;
+    }
+    return 0; 
+  };
 
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  Student.prototype.exclude = function(reason) {
+    this.subject = undefined;
+    this.marks = undefined;
+    this.excluded = reason;
+  };
